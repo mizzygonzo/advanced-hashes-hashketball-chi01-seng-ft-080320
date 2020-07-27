cbc2d50e3a5581190c0262c874f668be65566cda
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-27 03:46:12 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -126,4 +126,84 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(sought_player_name)
+  game_hash.each do |_place, team|
+    team.each do |attribute, data|
+      next unless attribute == :players
+
+      data.each do |player|
+        return player[:points] if player[:player_name] == sought_player_name
+      end
+    end
+  end
+end
+
+def shoe_size(sought_player_name)
+  game_hash.each do |_place, team|
+    team.each do |attribute, data|
+      next unless attribute == :players
+
+      data.each do |player|
+        return player[:shoe] if player[:player_name] == sought_player_name
+      end
+    end
+  end
+end
+
+def team_colors(team_name)
+  game_hash.each do |place, team|
+    return game_hash[place][:colors] if team[:team_name] == team_name
+  end
+end
+
+def team_names
+  game_hash.collect do |_place, team|
+    team[:team_name]
+  end
+end
+
+def player_numbers(team_name)
+  nums = []
+  game_hash.each do |_place, team|
+    next unless team[:team_name] == team_name
+
+    team.each do |attribute, data|
+      next unless attribute == :players
+
+      data.each do |data|
+        nums << data[:number]
+      end
+    end
+  end
+  nums
+end
+
+def player_stats(sought_player_name)
+  player = game_hash[:home][:players].find do |player|
+    player[:player_name] == sought_player_name
+  end
+
+  if player != nil
+    return player
+  end
+
+  game_hash[:away][:players].find do |player|
+    player[:player_name] == sought_player_name
+  end
+end
+
+def big_shoe_rebounds
+  biggest_shoe = 0
+  num_rebounds = 0
+
+  game_hash.each do |_team, game_data|
+    game_data[:players].each do |player|
+      if player[:shoe] > biggest_shoe
+        biggest_shoe = player[:shoe]
+        num_rebounds = player[:rebounds]
+      end
+    end
+  end
+
+  num_rebounds
+end
